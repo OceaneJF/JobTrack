f9ae8c8d6f3b5546800acf6bfb82b3f432ccac54
ojfrancois <ojfrancois@u-bordeaux.fr>
2024-08-22 16:45:36 +0200

    first commit

---
+++ back/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b840608407a4a6830d5c245635cd48354b8b6092',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b840608407a4a6830d5c245635cd48354b8b6092',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
