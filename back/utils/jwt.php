586fe0f18cdd91e00a8b3e0da4bf44e35bd061cd
ojfrancois <ojfrancois@u-bordeaux.fr>
2024-08-22 21:12:37 +0200

    adding login and signin route

--- back/utils/jwt.php
+++ back/utils/jwt.php
@@ -1,17 +1,38 @@
 <?php
 
+use Cassandra\Date;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
 require '../vendor/autoload.php';
-function getJWT():string{
-    $key = 'example_key';
+function getJWT(): string
+{
+    $env = parse_ini_file('../.env');
+    $key = $env['SECRET'];
     $payload = [
-        'iss' => 'http://example.org',
-        'aud' => 'http://example.com',
-        'iat' => 1356999524,
-        'nbf' => 1357000000
+        'iss' => 'https://jobtrack.oceane-jf.fr',
+        'aud' => 'https://jobtrack.oceane-jf.fr',
+        'iat' => time(),
+        'exp' => time() + 5 * 3600,
     ];
     $jwt = JWT::encode($payload, $key, 'HS256');
     return $jwt;
+}
+
+function expiredJWT(string $jwt): bool
+{
+    $env = parse_ini_file('../.env');
+    $key = $env['SECRET'];
+    $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+    $decoded_array = (array)$decoded;
+    return $decoded_array['iat'] > $decoded_array['exp'];
+}
+
+function decodeJWT(string $jwt): array
+{
+    $env = parse_ini_file('../.env');
+    $key = $env['SECRET'];
+    $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+    $decoded_array = (array)$decoded;
+    return $decoded_array;
 }
